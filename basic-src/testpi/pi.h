1e34c0af52859a513e74d1bf64354cb33617bde0
ZexinLee <551901204@qq.com>
2019-05-03 15:34:32 +0800

    Add testpi

---
+++ basic-src/testpi/pi.h
@@ -0,0 +1,31 @@
+/* StarPU --- Runtime system for heterogeneous multicore architectures.
+ *
+ * Copyright (C) 2012                                     Inria
+ * Copyright (C) 2010,2012,2015                           CNRS
+ * Copyright (C) 2010,2013-2014                           Université de Bordeaux
+ *
+ * StarPU is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or (at
+ * your option) any later version.
+ *
+ * StarPU is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * See the GNU Lesser General Public License in COPYING.LGPL for more details.
+ */
+
+#ifndef __PI_H__
+#define __PI_H__
+
+#include <starpu.h>
+#include <stdio.h>
+
+#define TYPE	float
+
+/* extern "C" void cuda_kernel(void *descr[], void *cl_arg); */
+
+static int n_dimensions = 100;
+
+#endif /* __PI_H__ */
